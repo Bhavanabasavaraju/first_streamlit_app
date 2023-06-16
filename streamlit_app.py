0cfbcd726e5b72c3cfbfd51544b4a06458436d59
Bhavanabasavaraju <65484606+Bhavanabasavaraju@users.noreply.github.com>
2023-06-16 18:09:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('Hello')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 and Bluberry Oatmeal')
+streamlit.text('kale, spinach and Rocket Smoothie')
+

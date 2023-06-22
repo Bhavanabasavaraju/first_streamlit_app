9d8ed420b611ab60b26dba26f793ab786570ec86
Bhavanabasavaraju <65484606+Bhavanabasavaraju@users.noreply.github.com>
2023-06-22 17:40:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,25 +6,16 @@ streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 and Bluberry Oatmeal')
 streamlit.text('kale, spinach and Rocket Smoothie')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+
+
+
 import pandas
-
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_fruit_list)
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
-my_fruit_list = my_fruit_list.set_index('Fruit')
-
-#fruits_to_show = my_fruit_list.loc[fruits_selected]
-streamlit.header("Fruityvice Fruit Advice!")
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-streamlit.dataframe(fruityvice_normalized)
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-import requests
-fruitvice_response = request.get("https://fruitvice.com/api/fruit/"fruit_choice)
+my_fruit_list = my_fruit_list.set_index('fruit')
 
 
+sreamlit.multiselect("pick some fruits:", list(my_fruit_list.index))
 
 
+streamlit.datafram(my_fruit_list)

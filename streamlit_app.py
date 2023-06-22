63b1820c88ad0fef677b40ebeab1614dbd13e114
Bhavanabasavaraju <65484606+Bhavanabasavaraju@users.noreply.github.com>
2023-06-22 17:27:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,29 +22,9 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
+import requests
+fruitvice_response = request.get("https://fruitvice.com/api/fruit/"fruit_choice)
 
-import snowflake.connector
 
-##my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-#my_cur = my_cnx.cursor()
-#my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-#my_data_row = my_cur.fetchone()
-#streamlit.text("Hello from Snowflake:")
-#streamlit.text(my_data_row)##
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from fruit_load_list")
-my_data_row = my_cur.fetchone()
-streamlit.text("The fruit load list contains:")
-streamlit.text(my_data_row)
-select * from pc_rivery_db.public.fruit_load_list
 
-my_cur.execute("select * from fruit_load_list")
-my_data_rows =my_cur.fetchall()
-streamlit.header("the fruit load list contains:")
-streamlit.dataframe(my_data_rows)
-
-streamlit.write('thanks for adding', add_my_fruit)
-my_cur.execute("insert into fruit_load_list values('from streamlit')")
-import snowflake.connector
